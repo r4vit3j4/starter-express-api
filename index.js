555f4817d0513edcee8119312538b6e5b1dc648a
Ravi Teja <raviteja281003@gmail.com>
2022-09-03 12:26:07 +0530

    Changed Code

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const http = require("http").Server(app);
+const PORT = process.env.port || 3000;
+const socketIO = require("socket.io")(http, {
+  cors: {
+    origin: "*",
+  },
+});
+
+app.use(cors());
+
+socketIO.on("connection", (socket) => {
+  console.log(`⚡: ${socket.id} user just connected!`);
+
+  socket.on("test", (socket) => {
+    socketIO.emit("test");
+  });
+
+  socket.on("disconnect", (socket) => {
+    console.log(`⚡: ${socket.id} user just disconnected!`);
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello Friend!");
+});
+
+http.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
